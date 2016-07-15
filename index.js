7b911a7a77b878543a90a1d1eece28022072b28c
Timothy <tbirrell@fahrenheit.io>
2016-07-15 13:04:31 -0500

    commented up code

--- index.js
+++ index.js
@@ -12,34 +12,45 @@ app.listen(5000, function(){
   console.log('App listening on port 5000!');
 });
 
+/**
+ * Listens for GET requests from extention
+ */
 app.get('/', function (req, res) {
-  if ('url' in req.headers) {
+  if ('url' in req.headers) { //verify url was sent
     getNotes(req.headers, res)
-  } else if ('url' in req.query) {
+  } else if ('url' in req.query) { //check backup location incase its being wierd
     getNotes(req.query, res)
   }
 });
 
+/**
+ * Listens for POST requests from extention
+ */
 app.post('/', function (req, res) {
-  if (req.body.api == 'post') {
+  if (req.body.api == 'post') { //check if insert/update
     setNotes(req.body, res);
-  } else if (req.body.api == 'delete') {
+  } else if (req.body.api == 'delete') { //check if delete
     killNotes(req.body, res);
   }
 });
 
+/**
+ * Gets notes from DB and sends to extention
+ */
 var getNotes = function(data, res) {
   MongoClient.connect(url, function(err, db) {
     var cursor = db.collection('devnotes').find({
       "url": data.url
     }).toArray(function(err, result){
-      // return results;
-      res.send(result);
+      res.send(result); // return results
     });
-    console.log("Retrived devnotes for " + data.url);
+    console.log("Retrived devnotes for " + data.url); //log action in terminal
   });
 }
 
+/**
+ * Insert or update note in DB
+ */
 var setNotes = function(data, res) {
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
@@ -58,21 +69,24 @@ var setNotes = function(data, res) {
         "upsert" : true
       }, function(err, count, result) {
          assert.equal(err, null);
-         console.log("Saved a devnote");
-         res.send("saved");
+         console.log("Saved a devnote"); //log action in terminal
+         res.send("saved"); //send response to extention
       }
     );
   });
 }
 
+/**
+ * Delete Note from DB
+ */
 var killNotes = function(data, res) {
   MongoClient.connect(url, function(err, db) {
     db.collection('devnotes').deleteOne({
       "id": data.id
     }, function(err, results) {
        assert.equal(err, null);
-       console.log("Deleted a devnote");
-       res.send("deleted");
+       console.log("Deleted a devnote"); //log action in terminal
+       res.send("deleted"); //send response to extention
     });
   });
 }
